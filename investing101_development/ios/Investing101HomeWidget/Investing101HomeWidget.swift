9b64c7759b30b2b2d43a90eab782f97afa22b881
leejungbin <leejungbin@operait.kr>
2026-06-09 13:52:25 +0900

    Add: Home widget background image

--- investing101_development/ios/Investing101HomeWidget/Investing101HomeWidget.swift
+++ investing101_development/ios/Investing101HomeWidget/Investing101HomeWidget.swift
@@ -2,33 +2,37 @@
 //  Investing101HomeWidget.swift
 //  Investing101HomeWidget
 //
-//  Created by LEE JUNG BIN on 6/9/26.
+//  Created by LEE JUNG BIN on 6/8/26.
 //
 
 import WidgetKit
 import SwiftUI
 
 struct Provider: AppIntentTimelineProvider {
+    
+    // Method to retrieve data from Flutter app
+    private func getDataFromFlutter() -> SimpleEntry{
+        let userDefaults = UserDefaults(suiteName: "group.homeScreenApp")
+        let textFromFlutterApp = userDefaults?.string(forKey: "text_from_flutter_app") ?? "0"
+        return SimpleEntry(date: Date(), text: textFromFlutterApp )
+    }
+    // Preview in widget gallery
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+        SimpleEntry(date: Date(), text: "0")
     }
 
+    
+    // Widget gallery/selection preview
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration)
+        SimpleEntry(date: Date(), text: "0")
     }
     
+    
+    // Actual widget on Home Screen
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        var entries: [SimpleEntry] = []
+       let entry = getDataFromFlutter()
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        return Timeline(entries: entries, policy: .atEnd)
+        return Timeline(entries: [entry], policy: .atEnd)
     }
 
 //    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
@@ -36,32 +40,44 @@ struct Provider: AppIntentTimelineProvider {
 //    }
 }
 
+
+// Data structure for my widget
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationAppIntent
+    let text: String
 }
 
+
+// The view that defines how our widget looks
 struct Investing101HomeWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
+            Text("My count:")
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+            Text(entry.text)
+                .foregroundColor(.white)
+                .font(.title)
+                .fontWeight(.bold)
         }
     }
 }
 
+
+// The main widget configuration
 struct Investing101HomeWidget: Widget {
     let kind: String = "Investing101HomeWidget"
 
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             Investing101HomeWidgetEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
+                .containerBackground(for: .widget) {
+                    Image("WidgetBackground")
+                        .resizable()
+                        .scaledToFill()
+                }
         }
     }
 }
@@ -83,6 +99,6 @@ extension ConfigurationAppIntent {
 #Preview(as: .systemSmall) {
     Investing101HomeWidget()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
-    SimpleEntry(date: .now, configuration: .starEyes)
+    SimpleEntry(date: .now, text: "0")
+    SimpleEntry(date: .now, text: "0")
 }
