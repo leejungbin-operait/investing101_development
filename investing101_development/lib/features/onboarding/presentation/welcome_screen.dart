477b316f7224c21452b40593a53a5c170e7c8268
leejungbin <leejungbin@operait.kr>
2026-06-10 10:16:07 +0900

    Add: Create WelcomeScreen class

---
+++ investing101_development/lib/features/onboarding/presentation/welcome_screen.dart
@@ -0,0 +1,159 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:investing101_development/shared/theme/colors.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const _SpeechBubble(),
+                  const SizedBox(height: 24),
+                  Image.asset(
+                    'assets/images/welcome_character.png',
+                    width: 180,
+                    height: 168,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              right: 20,
+              child: _ThreeDButton(
+                label: '반가워 베스티!',
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SpeechBubble extends StatelessWidget {
+  const _SpeechBubble();
+
+  static const _borderColor = Color(0xFFD4DBDE);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 335,
+      child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: _borderColor, width: 2),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Text(
+              '베스티에요.\n반가워요!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'TmoneyRoundWind',
+                fontWeight: FontWeight.w800,
+                fontSize: 14,
+                height: 20 / 14,
+                letterSpacing: -0.42,
+                color: Color(0xFF161F22),
+              ),
+            ),
+          ),
+          // Downward arrow pointer
+          Positioned(
+            bottom: -9,
+            child: Transform.rotate(
+              angle: 135 * math.pi / 180,
+              child: Container(
+                width: 10,
+                height: 10,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    top: BorderSide(color: _borderColor, width: 2),
+                    right: BorderSide(color: _borderColor, width: 2),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ThreeDButton extends StatelessWidget {
+  const _ThreeDButton({required this.label, required this.onTap});
+
+  final String label;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 52,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 48,
+              decoration: BoxDecoration(
+                color: const Color(0xFF4A40E9),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: onTap,
+              child: Container(
+                height: 48,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Center(
+                  child: Text(
+                    '반가워 베스티!',
+                    style: TextStyle(
+                      fontFamily: 'TmoneyRoundWind',
+                      fontWeight: FontWeight.w800,
+                      fontSize: 15,
+                      height: 22 / 15,
+                      letterSpacing: -0.45,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
