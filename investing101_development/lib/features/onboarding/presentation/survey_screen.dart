08ccd5bf4febaf520f25c6a0fb276a590c6baeee
leejungbin <leejungbin@operait.kr>
2026-06-10 16:51:36 +0900

    Add: Create SurveyScreen class

---
+++ investing101_development/lib/features/onboarding/presentation/survey_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:investing101_development/core/routes/app_routes.dart';
+import 'package:investing101_development/shared/widgets/onboarding_survey_screen.dart';
+
+class SurveyScreen extends StatelessWidget {
+  const SurveyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingSurveyScreen(
+      questionText: '주식에 대해서\n얼마나 알고 계시나요?',
+      progress: 0.1,
+      onNext: () {
+        Get.toNamed(Routes.survey);
+      },
+    );
+  }
+}
