93a1e5ad6d92ff804dd51dd0e860155297bf0276
leejungbin <leejungbin@operait.kr>
2026-06-11 14:43:35 +0900

    Add: Create WidgetInstallationScreen class

---
+++ investing101_development/lib/features/onboarding/presentation/widget_installation_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:investing101_development/shared/widgets/onboarding_animation_screen.dart';
+
+class WidgetInstallationScreen extends StatelessWidget {
+  const WidgetInstallationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingAnimationScreen(
+      progress: 0.825,
+      bubbleText: '홈 화면에서 인베스팅101\n학습 기록을 확인할 수 있어요!',
+      avatar: Image.asset('assets/images/quiz_avatar.png', fit: BoxFit.cover),
+      centerContent: Image.asset(
+        'assets/images/widget_install_phone.png',
+        width: 335,
+        height: 446,
+        fit: BoxFit.contain,
+      ),
+      primaryButtonLabel: '위젯 추가하기',
+      onPrimaryTap: () {},
+      secondaryButtonLabel: '나중에',
+      onSecondaryTap: () {},
+    );
+  }
+}
