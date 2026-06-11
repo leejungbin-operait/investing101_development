37aafa3c15453b9d6a4384d5fd085e71698841f3
leejungbin <leejungbin@operait.kr>
2026-06-11 16:22:30 +0900

    Add: Create appRouter file for listing all the routes with GoRouter

---
+++ investing101_development/lib/core/routes/app_router.dart
@@ -0,0 +1,43 @@
+import 'package:go_router/go_router.dart';
+import 'package:investing101_development/core/routes/app_routes.dart';
+import 'package:investing101_development/features/onboarding/presentation/permission_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/pre_quiz_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/splash_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/start_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/survey_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/welcome_screen.dart';
+import 'package:investing101_development/features/onboarding/presentation/widget_installation_screen.dart';
+
+final appRouter = GoRouter(
+  initialLocation: Routes.splash,
+  routes: [
+    GoRoute(
+      path: Routes.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: Routes.start,
+      builder: (context, state) => const StartScreen(),
+    ),
+    GoRoute(
+      path: Routes.welcome,
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute(
+      path: Routes.preQuiz,
+      builder: (context, state) => const PreQuizScreen(),
+    ),
+    GoRoute(
+      path: Routes.survey,
+      builder: (context, state) => const SurveyScreen(),
+    ),
+    GoRoute(
+      path: Routes.permission,
+      builder: (context, state) => const PermissionScreen(),
+    ),
+    GoRoute(
+      path: Routes.widgetInstallation,
+      builder: (context, state) => const WidgetInstallationScreen(),
+    ),
+  ],
+);
