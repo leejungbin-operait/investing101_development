6fb9247e627e9138d1640a5a70129cc749c52042
leejungbin <leejungbin@operait.kr>
2026-06-15 09:51:33 +0900

    Delete: Remove unused imports

--- investing101_development/lib/features/onboarding/presentation/widget_ios_screen.dart
+++ investing101_development/lib/features/onboarding/presentation/widget_ios_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:investing101_development/core/routes/app_routes.dart';
 import 'package:investing101_development/shared/widgets/onboarding_animation_screen.dart';
